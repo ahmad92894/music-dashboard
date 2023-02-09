5679760039b770ad75e221ae75d4316f75e9e8c0
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-09 14:45:29 -0600

    worked fetch request seatgeek page

--- assets/js/bandsintownscript.js
+++ assets/js/bandsintownscript.js
@@ -1,3 +1,35 @@
 $('#go-to-search').on('click', function(){
     window.location.replace('./index.html')
 })
+
+let URL="https://api.seatgeek.com/2/events?client_id=MzE4NDUyNTh8MTY3NTk2OTYzMS4wMDM2NTg4&client_secret=4fc93e4026f14705f22aa198d72446ccf014abbb3eed9e0e859909fc50868d0b"
+
+
+fetch(URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+    //   console.log(data)
+
+    let eventType=data.events;
+//    console.log(eventType);
+
+    let eventArray=[]
+    console.log(eventArray)
+
+    for (let i = 0; i < data.events.length; i++) {
+       let eventTypes=data.events[i].type;
+    //    console.log(eventTypes);
+        // console.log(eventTypes);
+        eventArray.push(eventTypes);
+
+        if(eventArray[i]==="concert") {
+            console.log("concert")
+        } else {
+            console.log('not concert')
+        }
+
+        
+        
+}})
