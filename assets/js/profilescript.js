6086e0a7a99cca8c280dfbc03f9c199896632a1d
Awele Lan <awelelan@Aweles-MacBook-Air.local>
2023-02-10 14:09:24 -0600

    edited files

--- assets/js/profilescript.js
+++ assets/js/profilescript.js
@@ -6,7 +6,6 @@ var usrSongs = JSON.parse(localStorage.getItem('userSongs')) || [];
 var codeBlock = $('#liked-songs');
 
 for(var i = 0; i < usrSongs.length; i++) {
-    //console.log(usrSongs[i]);
     var songString = usrSongs[i].name + " by " + usrSongs[i].artist.name;
     var addSong = $('<span>');
     addSong.text(songString);
@@ -14,13 +13,6 @@ for(var i = 0; i < usrSongs.length; i++) {
     var newline = $('<hr>')
     codeBlock.append(newline)
 }
-
-// var topArtists = JSON.parse(localStorage.getItem('topArtists')) || [];
-
-// for(var i = 0; i < topArtists.length; i++){
-//     // topArtist.artist.name
-//     console.log(topArtists[i].artist.name)
-// }
 
 var usrArtists = JSON.parse(localStorage.getItem('topArtists')) || [];
 var codeBlock2 = $('#top-Artists');
@@ -49,38 +41,3 @@ for(var i = 0; i < usrAlbum.length; i++) {
 
 
 
-//     // code for album below
-// $('#go-to-search').on('click', function(){
-//         window.location.replace('./index.html')
-//     })
-    
-//     var usrAlbum = JSON.parse(localStorage.getItem('userAlbum'));
-//     var codeBlock = $('#liked-album');
-    
-//     for(var i = 0; i < usrAlbum.length; i++) {
-//         //console.log(usrSongs[i]);
-//         var albumString = usrAlbum[i].name + " by " + usrAlbum[i].artist.name;
-//         var addSong = $('<span>');
-//         addSong.text(songString);
-//         codeBlock.append(addSong);
-//         var newline = $('<hr>')
-//         codeBlock.append(newline)
-
-// // code for favorite artist below
-// $('#go-to-search').on('click', function(){
-//             window.location.replace('./index.html')
-//         })
-        
-//         var usrfaveArtist = JSON.parse(localStorage.getItem('userfaveArtist'));
-//         var codeBlock = $('#liked-artist');
-        
-//         for(var i = 0; i < usrSongs.length; i++) {
-//             //console.log(usrSongs[i]);
-//             var songString = usrSongs[i].name + " by " + usrSongs[i].artist.name;
-//             var addSong = $('<span>');
-//             addSong.text(songString);
-//             codeBlock.append(addSong);
-//             var newline = $('<hr>')
-//             codeBlock.append(newline)
-
-// // code for playlist
