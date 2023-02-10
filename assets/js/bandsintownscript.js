c077c930c314ac3f3dc0330744ffe72cf71091b2
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-09 19:15:02 -0600

    added search bar

--- assets/js/bandsintownscript.js
+++ assets/js/bandsintownscript.js
@@ -4,6 +4,47 @@ $('#go-to-search').on('click', function(){
 
 let url="https://api.seatgeek.com/2/events?client_id=MzE4NDUyNTh8MTY3NTk2OTYzMS4wMDM2NTg4&client_secret=4fc93e4026f14705f22aa198d72446ccf014abbb3eed9e0e859909fc50868d0b"
 
+// let navEl=$("<nav>");
+// let navWrapperDiv=$("<div>");
+//     navWrapperDiv.addClass("nav-wrapper");
+// let formEl=$("<form>");
+// let inputDiv=$("<div>");
+//     inputDiv.addClass("input-field");
+// let searchBar=$("<input>");
+//     searchBar.attr("id", "search");
+//     searchBar.attr("type", "search");
+//     searchBar.attr("placeholder", "Search Here!")
+//     searchBar.attr("required", true);
+// let labelEl=$("<label>");
+//     labelEl.addClass("label-icon");
+//     labelEl.attr("for", "search");
+// let anchorEl=$("<a>");
+//     anchorEl.addClass("waves-effect waves-light btn-large")
+//     anchorEl.attr("id", "search-button");
+//     anchorEl.text("Search")
+// let iconEl=$("<i>");
+//     iconEl.addClass("material-icons");
+//     iconEl.text("search");
+// let iconEl2=$("<i>");
+//     iconEl2.addClass("material-icons");
+//     iconEl2.text("close");
+
+// labelEl.append(iconEl);
+// labelEl.append(iconEl2);
+// labelEl.append(anchorEl);
+// inputDiv.append(labelEl);
+// inputDiv.append(searchBar);
+// formEl.append(inputDiv);
+// navWrapperDiv.append(formEl);
+// navEl.append(navWrapperDiv);
+// $("#search-bar-1").append(navEl);
+
+// $('#search-button').on("click", function(event) {
+//     event.preventDefault();
+//     let userInput=$("#search").val();
+//     console.log(userInput);
+// }
+// )
 
 fetch(url)
     .then(function (response) {
@@ -29,11 +70,7 @@ fetch(url)
         } else {
             console.log('not concert')
         }
-
-    let navEl=$("<nav>");
-    let navWrapperEl=$("<div>")
-        navWrapperEl.addClass("nav-wrapper")
-    let form=$("<form>") 
+            //search bar 1
         
 }})
 
