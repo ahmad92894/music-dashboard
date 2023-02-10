8192cef2a1d7f857a3ddedbe0fc6af72c155d956
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-10 10:52:43 -0600

    continued on seatgeek table

--- assets/js/seatgeekscript.js
+++ assets/js/seatgeekscript.js
@@ -30,19 +30,24 @@ fetchButton.addEventListener('click', function(event){
             console.log(data);
             console.log(data.recommendations)
             var eventArray = data.recommendations;
-         
+        
+        
         let tableHeadEl=$("<thead>");
         let tableRowEl=$("<tr>");
         let tableHeader1=$("<th>");
             tableHeader1.text("Artist")
+            tableHeader1.attr("id", "table-header-1" )
         let tableHeader2=$("<th>");
             tableHeader2.text("Venue");
+            tableHeader2.attr("id", "table-header-2")
         let tableHeader3=$("<th>");
             tableHeader3.text("Local Time");
+            tableHeader3.attr("id", "table-header-3")
         let tableRow1=$("<tr>");
         let tableRow2=$("<tr>");
         let tableRow3=$("<tr>");
-        let tableBody=$("<tbody>")
+        let tableBody=$("<tbody>");
+     
       
         tableRowEl.append(tableHeader1);
         tableRowEl.append(tableHeader2);
@@ -57,32 +62,61 @@ fetchButton.addEventListener('click', function(event){
         tableBody.append(tableRow3);
         
 
-            for(var i= 0;i<eventArray.length;i++){
+            for(var i= 0; i < eventArray.length;i++){
                 //console.log(eventArray[index].event); //event
                 var nameOfEvent = eventArray[i].event.title //event.title
                 var nameOfVenue = eventArray[i].event.venue.name 
                 var localStartTime = eventArray[i].event.datetime_local
                 var timeFormatted = dayjs(localStartTime).format('h:mmA ddd, MMM D, YYYY')
 
-            let tableDataArtist=$("<td>")
-            tableDataArtist.text=(nameOfEvent);
             
-            tableRow1.append(tableDataArtist);
+        // let tdRowContainer=$("<div>");
+        //     tdRowContainer.attr("class", "row");
+            let tdContainer1=$("<div>");
+            tdContainer1.attr("class", "col");
+            let tableDataArtist=$("<td>");
+            tableDataArtist.text(nameOfEvent);
+            console.log(tableDataArtist);
+                
+            tdContainer1.append(tableDataArtist);
+            tableRow1.append(tdContainer1)
             tableBody.append(tableRow1);
             $("#table").append(tableBody);
+            
+            let tdContainer2=$("<div>");
+            tdContainer2.attr("class", "col push-s6");
+        
+            let tableDataVenue=$("<td>")
+            tableDataVenue.text(nameOfVenue);
+            console.log(tableDataVenue);
+    
+            tdContainer2.append(tableDataVenue);
+            tableRow2.append(tdContainer2)
+            tableBody.append(tableRow2);
+            $("#table").append(tableBody);
 
+            let tdContainer3=$("<div>");
+            tdContainer3.attr("class", "col");
+            let tableDataTime=$("<td>")
+            tableDataTime.text(localStartTime);
+            console.log(tableDataTime);
+    
+            tdContainer3.append(tableDataTime);
+            tableRow3.append(tdContainer3)
+            tableBody.append(tableRow3);
+            $("#table").append(tableBody);
+            
            
-            
-
 
             
-        
-        
-        
+            // tableRow1.append(tableDataArtist);
+            // tableBody.append(tableRow1);
+            // $("#table").append(tableBody);
 
-                // var listItem = document.createElement('li');
-                // listItem.textContent = nameOfEvent + " at the " + nameOfVenue + " starting at " + timeFormatted + " local time";
-                // tableBody.appendChild(listItem);
+        
+            // var listItem = document.createElement('li');
+            // listItem.textContent = nameOfEvent + " at the " + nameOfVenue + " starting at " + timeFormatted + " local time";
+            // tableBody.appendChild(listItem);
 
          
             
