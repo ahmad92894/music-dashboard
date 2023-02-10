d8323407931c8ba2c7bd4066ed1fb270f28b19c2
Awele Lan <awelelan@Aweles-MacBook-Air.local>
2023-02-10 09:46:58 -0600

    js file draft

--- assets/js/profilescript.js
+++ assets/js/profilescript.js
@@ -13,4 +13,46 @@ for(var i = 0; i < usrSongs.length; i++) {
     codeBlock.append(addSong);
     var newline = $('<hr>')
     codeBlock.append(newline)
-}
+
+
+
+
+
+//     // code for album below
+// $('#go-to-search').on('click', function(){
+//         window.location.replace('./index.html')
+//     })
+    
+//     var usrAlbum = JSON.parse(localStorage.getItem('userAlbum'));
+//     var codeBlock = $('#liked-album');
+    
+//     for(var i = 0; i < usrAlbum.length; i++) {
+//         //console.log(usrSongs[i]);
+//         var albumString = usrAlbum[i].name + " by " + usrAlbum[i].artist.name;
+//         var addSong = $('<span>');
+//         addSong.text(songString);
+//         codeBlock.append(addSong);
+//         var newline = $('<hr>')
+//         codeBlock.append(newline)
+
+// // code for favorite artist below
+// $('#go-to-search').on('click', function(){
+//             window.location.replace('./index.html')
+//         })
+        
+//         var usrfaveArtist = JSON.parse(localStorage.getItem('userfaveArtist'));
+//         var codeBlock = $('#liked-artist');
+        
+//         for(var i = 0; i < usrSongs.length; i++) {
+//             //console.log(usrSongs[i]);
+//             var songString = usrSongs[i].name + " by " + usrSongs[i].artist.name;
+//             var addSong = $('<span>');
+//             addSong.text(songString);
+//             codeBlock.append(addSong);
+//             var newline = $('<hr>')
+//             codeBlock.append(newline)
+
+// // code for playlist
+        
+        
+    
