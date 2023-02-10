db82d5f2d80764dd479931b3eeef99871436a57f
Awele Lan <awelelan@Aweles-MacBook-Air.local>
2023-02-10 11:15:00 -0600

    added new files

--- assets/js/profilescript.js
+++ assets/js/profilescript.js
@@ -13,8 +13,39 @@ for(var i = 0; i < usrSongs.length; i++) {
     codeBlock.append(addSong);
     var newline = $('<hr>')
     codeBlock.append(newline)
+}
+
+// var topArtists = JSON.parse(localStorage.getItem('topArtists')) || [];
+
+// for(var i = 0; i < topArtists.length; i++){
+//     // topArtist.artist.name
+//     console.log(topArtists[i].artist.name)
+// }
+
+var usrArtists = JSON.parse(localStorage.getItem('topArtists')) || [];
+var codeBlock2 = $('#top-Artists');
+
+for(var i = 0; i < usrArtists.length; i++) {
+    var artistStr = i+1 + ". " + usrArtists[i].artist.name;
+    var addArtist = $('<span>');
+    addArtist.text(artistStr);
+    codeBlock2.append(addArtist);
+    var newline = $('<hr>');
+    codeBlock2.append(newline)
+}
 
 
+var usrAlbum = JSON.parse(localStorage.getItem('userAlbums')) || [];
+var codeBlock3 = $('#top-Album');
+
+for(var i = 0; i < usrArtists.length; i++) {
+    var albumStr = i+1 + ". " + usrAlbum[i].album.name;
+    var addAlbum = $('<span>');
+    addAlbum.text(albumStr);
+    codeBlock3.append(addAlbum);
+    var newline = $('<hr>');
+    codeBlock3.append(newline)
+}
 
 
 
@@ -53,6 +84,3 @@ for(var i = 0; i < usrSongs.length; i++) {
 //             codeBlock.append(newline)
 
 // // code for playlist
-        
-        
-    
