1bdfcd60cfd0e08da30d1605af326eb9cc4a94d1
dmanaglia <ddsmm.managlia@gmail.com>
2023-02-09 20:23:18 -0600

    printed user songs to profile page

--- assets/js/profilescript.js
+++ assets/js/profilescript.js
@@ -3,8 +3,14 @@ $('#go-to-search').on('click', function(){
 })
 
 var usrSongs = JSON.parse(localStorage.getItem('userSongs'));
+var codeBlock = $('#liked-songs');
 
 for(var i = 0; i < usrSongs.length; i++) {
     //console.log(usrSongs[i]);
-    console.log(usrSongs[i].name + " by " + usrSongs[i].artist.name);
+    var songString = usrSongs[i].name + " by " + usrSongs[i].artist.name;
+    var addSong = $('<span>');
+    addSong.text(songString);
+    codeBlock.append(addSong);
+    var newline = $('<hr>')
+    codeBlock.append(newline)
 }
